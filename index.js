2da865bea781267a44079473935298911204cfac
Michael Chung <mikechung89@gmail.com>
2022-02-24 13:59:36 -0500

    commit tree 6ff3c0c130b244c4f0e7be2c32d42f4ccded2cb1

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(array, string) {
+    return array.filter(word => word.toLowerCase() === string.toLowerCase())
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter(word => word.substring(0,2).indexOf(string) !== -1)
+}
+
+function matchName(array, string) {
+    return array.filter(word => word.name === string)
+}
